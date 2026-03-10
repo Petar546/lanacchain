6621a5d3734a4077b85d46c014d14cc6cc580b4a
Petar546 <petar@dawnn.info>
2026-03-10 15:15:07 +0100

    assert commitToLocalChain size and validity

--- src/test/java/com/kameni/lanacchain/peer/PeerTest.java
+++ src/test/java/com/kameni/lanacchain/peer/PeerTest.java
@@ -1,23 +1,15 @@
 package com.kameni.lanacchain.peer;
 
-import com.kameni.lanacchain.exceptions.LanacDeserializationException;
 import com.kameni.lanacchain.exceptions.LanacSignatureException;
 import com.kameni.lanacchain.lanac.Lanac;
 import com.kameni.lanacchain.lanac.data.LanacData;
 import com.kameni.lanacchain.lanac.data.SignedAction;
-import com.kameni.lanacchain.testrunner.LanacTestUtils;
 import com.kameni.lanacchain.testrunner.annotations.Test;
 import com.kameni.lanacchain.testrunner.annotations.TestClass;
 
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
-import static com.kameni.lanacchain.testrunner.LanacAssert.*;
+import static com.kameni.lanacchain.testrunner.LanacAssert.assertTrue;
 
 @TestClass
 public class PeerTest {
@@ -62,10 +54,8 @@ public class PeerTest {
         List<SignedAction> unsorted = List.of(actionA, actionB);
         List<SignedAction> sorted = peer.sortActions(unsorted);
 
-        String first = sorted.get(0).getPeerAddress();
-        String second = sorted.get(1).getPeerAddress();
+        peer.commitToLocalChain(unsorted);
         Lanac lanac = peer.getLanac();
-        assertTrue(lanac.isChainValid(), "Chain isnt valid");
 
         for (int i = 0; i < lanac.getBlockchainSize(); i++) {
             String blockDisplay = String.format("Block[%d] Hash: %s | Prev: %s",
@@ -76,9 +66,10 @@ public class PeerTest {
 
             IO.println(blockDisplay);
         }
+        assertTrue(lanac.isChainValid(), "Chain isnt valid");
+        assertTrue(lanac.getBlockchainSize() == 3, "Blockchain size doesnt match");
 
-        // Lexicographical check: ensures all peers process in alphabetical order
-        assertTrue(first.compareTo(second) <= 0, "Blockchain determinism failed: Peer addresses are not sorted");
+
 
     }
 
